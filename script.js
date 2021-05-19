6a4db77ba5359dfaca54231cf8494c85db5afd30
jpruski <72477191+jpruski@users.noreply.github.com>
2021-05-19 04:19:16 -0400

    need to run this to test

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-var centers = (function () {
-  var seq3 = [0, 1, 2];
+var centers = (function ( seq3 ) {
   var gcd = function(a,b) b===0 ? a : gcd(b,a%b);
   var simplify = function(coord) {
     var gcf = Math.abs( gcd(coord[0], gcd( coord[0],coord[0] )));
     return [ coord[0]/gcf, coord[1]/gcf, coord[2]/gcf ];
   };
-  var getlen2 = function( deltas ) seq3.reduce(
+  var getLen2 = function( deltas ) seq3.reduce(
     function( acc, i ) acc - (deltas[ (i+1)%3 ] * deltas[ i ]),
     0 );
   var centers = function(sides2) {
@@ -19,28 +18,13 @@ var centers = (function () {
     ];
   };
   return function(point) {
-    // to do
-  }
-})();
+    var s2 = point[0] * point[0];
+    var len2s = seq3.map(function(corner) getLen2(
+      seq3.map(function(coord) coord===corner ? 
+        point[coord+1] - point[0] 
+        : point[coord+1])));
+    return seq3.map(function(corner) [s2, len2s[(corner+1)%3], len2s[(corner+2)%3]]);
+  };
+})( [0, 1, 2] );
 
-// let centers =
-//   let rec gcd (a:int64) (b:int64) = if b=0L then a else gcd b (a % b)
-//   let simplify (coord:int64[]) =
-//      let gcf = abs (gcd coord.[0] (gcd coord.[1] coord.[2]))
-//      [|coord.[0]/gcf; coord.[1]/gcf; coord.[2]/gcf|]
-//   let getlen2 = fun (d:int64[]) -> Seq.sum (seq { for i in 0..2 do -d.[(i+1)%3]*d.[i] }) |]
-//   let centers (s2:int64[]) =
-//      let heron = [| for coord in 0..2 do Seq.sum (seq { for i in 0..2 do if i=coord then -s2.[i] else s2.[i] }) |]
-//      [|
-//         Array.map (fun (n:int64) -> (int64 (sqrt (float n)))) s2;
-//         simplify [| for i in 0..2 do s2.[i]*heron.[i] |];
-//         simplify [| for i in 0..2 do heron.[(i+1)%3]*heron.[(i+2)%3] |]
-//      |]  
-// ]]   fun (point:int64[]) ->
-//      let s2 = point.[0]*point.[0]
-//      let len2s =  [|
-//         for corner in 0..2 do
-//            getlen2 [| for coord in 0..2 do if coord=corner then point.[coord+1]-point.[0] else point.[coord+1] |]
-//      |]
-//      [| for corner in 0..2 do centers [| s2; len2s.[(corner+1)%3]; len2s.[(corner+2)%3] |] |]
-// printfn "%A" (centers [|   784L ;   325L ;   264L ;  195L |])
+console.log(centers([ 784,   325,   264,  195 ]));
